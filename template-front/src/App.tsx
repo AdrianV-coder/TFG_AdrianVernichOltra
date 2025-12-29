28e587f6e091e361cad69254099614f15922628e
AdrianV-coder <vernicholtraadrian@gmail.com>
2025-12-29 20:17:10 +0100

    Changes due to erroneous modifications on GitHub

--- template-front/src/App.tsx
+++ template-front/src/App.tsx
@@ -1,6 +1,3 @@
-<<<<<<< Updated upstream
-function App() {
-=======
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { ProtectedRoute } from "./routes/ProtectedRoute";
 import LoginPage from "./pages/LoginPage";
@@ -12,23 +9,14 @@ import CreateHabitPage from "./pages/CreateHabitPage";
 import EditHabitPage from "./pages/EditHabitPage";
 import { Provider } from "react-redux";
 import { store } from "./store";
->>>>>>> Stashed changes
 
 export default function App() {
   return (
-<<<<<<< Updated upstream
-    <>
-      <h1>Hello, World!</h1>
-    </>
-  )
-}
-=======
     <Provider store={store}>
       <Router>
         <Routes>
           <Route path="/" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
->>>>>>> Stashed changes
 
           <Route
             path="/home"
